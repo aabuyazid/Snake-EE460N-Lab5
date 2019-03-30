9e773df1c17c00415a5c773feeac76dfb6c931f8
Arkan Abuyazid <arkan.2.abuyazid@gmail.com>
2019-03-30 16:41:21 -0500

    modified:   SnakeGame/Snake_SM.v

--- SnakeGame/Snake_SM.v
+++ SnakeGame/Snake_SM.v
@@ -39,6 +39,106 @@
 `define RIGHT3 22
 `define RIGHTPAUSE 23
 
+module hexto7segment(
+    input [3:0] x,
+    output reg [6:0] r
+    );
+    
+    always @ (*)
+        case (x)
+            4'b0000: r = 7'b0000001;
+            4'b0001: r = 7'b1001111;
+            4'b0010: r = 7'b0010010;
+            4'b0011: r = 7'b0000110;
+            4'b0100: r = 7'b1001100;
+            4'b0101: r = 7'b0100100;
+            4'b0110: r = 7'b0100000;
+            4'b0111: r = 7'b0001111;
+            4'b1000: r = 7'b0000000;
+            4'b1001: r = 7'b0000100;
+            4'b1010: r = 7'b0001000;
+            4'b1011: r = 7'b1100000;
+            4'b1100: r = 7'b0110001;
+            4'b1101: r = 7'b1000010;
+            4'b1110: r = 7'b0110000;
+            4'b1111: r = 7'b0111000;
+        endcase
+        
+endmodule
+
+module SSEGClkDivider(
+    input clk,
+    output SSEGClk
+);
+
+reg [18:0] COUNT; // Arbitrary number, GO CHANGE IT
+    
+assign SSEGClk = COUNT[18];
+    
+always @(posedge clk) begin
+    COUNT = COUNT + 1;
+end
+
+endmodule
+
+module StateDisplay(
+    input SSEGClk,
+    input [13:0] sev_seg_data,
+    input KeyRelease,
+    output reg [3:0] an,
+    output reg [6:0] sseg
+    );
+    
+    reg [1:0] state = 2'b11;
+    reg [1:0] next_state;
+    
+    always @ (*) begin
+        case (state)
+            2'b00: begin
+                an = 4'b1101; 
+                sseg = sev_seg_data[13:7];
+                next_state = 2'b01;
+            end
+            2'b01: begin
+                an = 4'b1110; 
+                sseg = sev_seg_data[6:0];
+                next_state = 2'b00;
+            end
+            default: begin
+                if(KeyRelease)
+                    next_state = 2'b00;
+                else
+                    next_state = 2'b11;
+                an = 4'b1111;
+                sseg = 7'b1111111;
+            end
+        endcase
+    end
+
+    always @(posedge SSEGClk) begin
+        state <= next_state;
+    end
+endmodule
+
+module StateDisplayController(
+    input clk,
+    input [7:0] curr_state,
+    output [3:0] an,
+    output [6:0] sseg
+);
+
+wire [13:0] sev_seg_data;
+wire SSEGClk;
+
+SSEGClkDivider s1 (clk,SSEGClk);
+
+hexto7segment left  (curr_state[7:4],sev_seg_data[13:7]);
+hexto7segment right (curr_state[3:0],sev_seg_data[6:0]);
+
+StateDisplay display (SSEGClk,sev_seg_data,an,sseg);
+
+endmodule
+
 module clk_div(
     input main_clk,
     output reg game_clk
@@ -73,7 +173,9 @@ module Snake_SM(
     output [6:0] SnakePos2_Y,
     output [6:0] SnakePos3_X,
     output [6:0] SnakePos3_Y,
-    output reg AllBlack
+    output reg AllBlack,
+    output [3:0] an,
+    output [6:0] sseg
 );
 
 wire [7:0] KeyPress;
@@ -91,14 +193,16 @@ reg [1:0] next_head;
  
 reg [2:0] index;
 
-assign SnakePos0_X = SnakePos[0];
-assign SnakePos0_Y = SnakePos[1];
-assign SnakePos1_X = SnakePos[2];
-assign SnakePos1_Y = SnakePos[3];
-assign SnakePos2_X = SnakePos[4];
-assign SnakePos2_Y = SnakePos[5];
-assign SnakePos3_X = SnakePos[6];
-assign SnakePos3_Y = SnakePos[7];
+StateDisplayController con (main_clk,curr_state,an,sseg);
+
+assign SnakePos0_Y = SnakePos[0];
+assign SnakePos0_X = SnakePos[1];
+assign SnakePos1_Y = SnakePos[2];
+assign SnakePos1_X = SnakePos[3];
+assign SnakePos2_Y = SnakePos[4];
+assign SnakePos2_X = SnakePos[5];
+assign SnakePos3_Y = SnakePos[6];
+assign SnakePos3_X = SnakePos[7];
 
 initial begin
     curr_state <= `WAIT;
@@ -141,6 +245,8 @@ always@(posedge main_clk) begin
                 SnakePos[index+index+1] <= 23; // y-coor
             end
             next_head <= 3;
+            next_tail <= 0;
+            AllBlack <= 0;
         end
         `BLACK: begin
             if(KeyPress == `START)
