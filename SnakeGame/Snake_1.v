ecefbff2a8abeb0cd29a2f3964fb94624c9b2ab3
VicAF <47991761+VicAF@users.noreply.github.com>
2019-04-01 22:29:18 -0500

    Update Snake_1.v

--- SnakeGame/Snake_1.v
+++ SnakeGame/Snake_1.v
@@ -1,41 +1,23 @@
 `timescale 1ns / 1ps
 `define  sCol  12'h0F0;
 `define  bCol  12'hFFF;
-`define  black  12'h000;
-module Snake_1(clk, Py1, Py2, Py3, Py4, Px1, Px2, Px3, Px4, hIndex,vIndex,color,AllBlack);
+`define black  12'h000;
+module Snake_1(clk, Px1, Py1, Px2, Py2, Px3, Py3, Px4, Py4, hIndex,vIndex,color,AllBlack);
     input clk, AllBlack;
-    input [6:0] hIndex, vIndex;
-    input [6:0] Py1, Py2, Py3, Py4, Px1, Px2, Px3, Px4;
+    input [5:0] hIndex, vIndex;
+    input [5:0] Px1, Py1, Px2, Py2, Px3, Py3, Px4, Py4;
     output [11:0] color;
-    wire clkFPS, clk25MHz;
-    integer i,j;
-    reg [63:0] screen [47:0];
+    wire clk25MHz;
     reg [11:0] colorReg;
-    clkFPS s6 (clk, clkFPS);
     clk4th s7 (clk, clk25MHz);
-    
-    //Refreshes the screen and places the snake 
-    always@(posedge clkFPS) begin
-//        for (i=0; i<48; i=i+1) begin
-//            for (j=0; j<64; j=j+1) begin
-//                screen[i][j] <= (i == Py1 && j == Px1) || 
-//                                (i == Py2 && j == Px2) ||
-//                                (i == Py3 && j == Px3) ||
-//                                (i == Py4 && j == Px4);
-//            end
-//        end
-        for (i=0; i<48; i=i+1) begin
-            screen[i] = 0;
-        end
-        screen[vIndex][hIndex] = 1'b1;
-    end
+
     //maps the screen to the output
     assign color = colorReg;
     always@(posedge clk25MHz) begin
-        if(AllBlack) begin
+        if (AllBlack) begin
             colorReg <= `black;
         end else begin
-            if(screen[vIndex][hIndex]) begin
+            if((vIndex == Py1 && hIndex == Px1) || (vIndex == Py2 && hIndex == Px2) || (vIndex == Py3 && hIndex == Px3) || (vIndex == Py4 && hIndex == Px4)) begin
                 colorReg <= `sCol;
             end else
                 colorReg <= `bCol;
@@ -43,15 +25,16 @@ module Snake_1(clk, Py1, Py2, Py3, Py4, Px1, Px2, Px3, Px4, hIndex,vIndex,color,
     end
 endmodule
 
-module clkFPS (clk, clkFPS); // 5kHz
+module clkFPS (clk, clkFPS);
     input clk; 
-    output clkFPS;
-    reg [19:0] count=0;
-    reg game_clk = 0;
-    assign clkFPS = game_clk;
-    always @(posedge clk) begin
-        if(count == 10000000) begin
-            game_clk <= ~game_clk;
+    output reg clkFPS;
+    reg [24:0] count=0;
+    initial
+        clkFPS=0;
+    
+    always@(posedge clk) begin
+        if (count == 25'd10000000) begin
+            clkFPS = ~clkFPS;
             count <= 0;
         end else
             count <= count + 1;
